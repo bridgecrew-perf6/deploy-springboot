564cb96f2870adb67459c04ae58ba949f08c3cb4
tosumaso <wakuwaku5712@gmail.com>
2022-02-28 13:19:41 +0900

    first commit

---
+++ src/main/java/awsdeploy/AwsDeploySpringbootApplication.java
@@ -0,0 +1,13 @@
+package awsdeploy;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AwsDeploySpringbootApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(AwsDeploySpringbootApplication.class, args);
+	}
+
+}
---
+++ src/test/java/awsdeploy/AwsDeploySpringbootApplicationTests.java
@@ -0,0 +1,13 @@
+package awsdeploy;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AwsDeploySpringbootApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
