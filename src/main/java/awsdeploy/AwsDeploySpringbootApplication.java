7560ab70646f78365113a5848937b3ed7c2d80d4
tosumaso <wakuwaku5712@gmail.com>
2022-03-04 18:04:34 +0900

    war config

--- src/main/java/awsdeploy/AwsDeploySpringbootApplication.java
+++ src/main/java/awsdeploy/AwsDeploySpringbootApplication.java
@@ -2,12 +2,19 @@ package awsdeploy;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.builder.SpringApplicationBuilder;
+import org.springframework.boot.web.servlet.support.SpringBootServletInitializer;
 
 @SpringBootApplication
-public class AwsDeploySpringbootApplication {
+public class AwsDeploySpringbootApplication extends SpringBootServletInitializer{
 
 	public static void main(String[] args) {
 		SpringApplication.run(AwsDeploySpringbootApplication.class, args);
 	}
+	
+	@Override
+    protected SpringApplicationBuilder configure(SpringApplicationBuilder application) {
+        return application.sources(AwsDeploySpringbootApplication.class);
+    }
 
 }
